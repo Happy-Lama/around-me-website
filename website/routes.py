6c8f8b5ddcccfd1747d802c43d2a5e46499fe969
Kissa Leon Denis <kldenis2001@gmail.com>
2021-10-23 19:23:08 +0300

    New changes

--- website/routes.py
+++ website/routes.py
@@ -1,9 +1,39 @@
 from website import app
-from flask import url_for, render_template
+from flask import url_for, render_template, redirect
+placecards = [
+    {
+        'name': "Castle Royale",
+        'location': "Kampala",
+        'distance_from_current_location': 124,
+        'rating': 4.5,
+        'description': "A fun and lovely place to hangout with family.\nWe serve an assortment of drinks and foods.",
+        'img_src': '#'
+    },
+    {
+        'name': "Castle Royale",
+        'location': "Namboole",
+        'distance_from_current_location': 124,
+        'rating': 4.5,
+        'description': "A fun and lovely place to hangout with family.\nWe serve an assortment of drinks and foods.",
+        'img_src': '#'
+    },
+    {
+        'name': "Castle Royale",
+        'location': "Lugogo",
+        'distance_from_current_location': 124,
+        'rating': 4.5,
+        'description': "A fun and lovely place to hangout with family.\nWe serve an assortment of drinks and foods.",
+        'img_src': '#'
+    }
 
+]
 @app.route("/")
 @app.route("/index")
 @app.route("/home")
 def index():
-    return render_template("index.html")
+    return render_template("index.html",placecards=placecards)
+
+
+
+
 
---
+++ website/static/js/script.js
@@ -0,0 +1,8 @@
+function find_places(category){
+    console.log(category);
+}
+
+function place(elmnt){
+    console.log(elmnt)
+}
+
